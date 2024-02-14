08d6301353e208d7ba7eb66650a6a086b40c7692
kishorebolt03 <kishorebolt60@gmail.com>
2024-02-14 09:22:32 +0530

    syntax error fixed

--- printIP/lambda_function.py
+++ printIP/lambda_function.py
@@ -1,11 +1,12 @@
 def handler(event, context):
-    method_type=str(event['requestContext']['http']['method'])
+    print(event)
+    #method_type=str(event['requestContext']['http']['method'])
     if (method_type == 'GET'):
         ip_address = event.get('requestContext', {}).get('headers', {}).get('X-Forwarded-For', '')
         print(f"Requester IP: {ip_address}")
         # Your function logic here
         return {
-                'statusCode':200
+                'statusCode':200,
                 'body': json.dumps(f"Requester IP : {ip_address}")
                 }
 
