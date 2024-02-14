9951a156ceb2d2209f715d979faaa864514cac8b
kishorebolt03 <kishorebolt60@gmail.com>
2024-02-14 14:11:00 +0530

    Final changes

--- printIP/lambda_function.py
+++ printIP/lambda_function.py
@@ -2,12 +2,12 @@ import json
 from flask import Flask, render_template
 
 def handler(event, context):
-    print(event)
-    print("Modified")
+    #print(event)
+    #print("Modified")
     method_type=str(event['httpMethod'])
     if (method_type == 'GET'):
         ip_address = event.get('headers', {}).get('X-Forwarded-For', '')
-        print(f"Requester IP: {ip_address}")
+        #print(f"Requester IP: {ip_address}")
         # Your function logic here
         return {
                 'statusCode':200,
