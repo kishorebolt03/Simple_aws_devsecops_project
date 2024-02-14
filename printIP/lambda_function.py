45ce8756ac73f2b526df834e7b3523d89f7ae073
kishorebolt03 <kishorebolt60@gmail.com>
2024-02-14 07:47:23 +0530

    modified lambda code and added response code

--- printIP/lambda_function.py
+++ printIP/lambda_function.py
@@ -1,8 +1,14 @@
 def handler(event, context):
-  ip_address = event.get('requestContext', {}).get('headers', {}).get('X-Forwarded-For', '')
-  print(f"Requester IP: {ip_address}")
-  # Your function logic here
-  return {}
+    method_type=str(event['requestContext']['http']['method'])
+    if (method_type == 'GET'):
+        ip_address = event.get('requestContext', {}).get('headers', {}).get('X-Forwarded-For', '')
+        print(f"Requester IP: {ip_address}")
+        # Your function logic here
+        return {
+                'statusCode':200
+                'body': json.dumps(f"Requester IP : {ip_address}")
+                }
 
 if __name__ == "__main__":
     pass
+
